8b6938f67af5f235ce6fefd559b8111519d804bf
Gabanger <gabighost3491@gmail.com>
2025-07-24 12:22:33 +0200

    Test textures for player + 3d person camera (without raycasting)

--- scenes/camera_3d.gd
+++ scenes/camera_3d.gd
@@ -1,14 +1,32 @@
 extends Camera3D
 
 const SENSITIVITY = 1.0/240.0
+const THIRD_PERSON_RANGE = 5
+const OFFSET_POS = Vector3(0,1.25,0)
+
+var is_in_first_person = true
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
-		var MouseEvent = event.relative
+		update_camera(event,true)
+	if event is InputEventKey:
+		if event.is_action_pressed("Switch camera person"):
+			is_in_first_person = not is_in_first_person
+			update_camera(event,false)
+
+func update_camera(e : InputEvent, change_angle : bool):
+	if change_angle:
+		var MouseEvent = e.relative
 		rotation.x += MouseEvent.y * SENSITIVITY * -1
 		rotation.x = min(PI/2,rotation.x)
 		rotation.x = max(-PI/2,rotation.x)
 		rotation.y += MouseEvent.x * SENSITIVITY * -1
+	if is_in_first_person:
+		position = OFFSET_POS
+	else:
+		var vec = global_basis * Vector3.FORWARD
+		vec *= -1 * THIRD_PERSON_RANGE
+		position = OFFSET_POS + vec
