eff4c0d52ec0dd4c7efe112e582ad291eb73a807
ar1hurgit <arthurdk0805@gmail.com>
2025-07-25 10:57:30 +0200

    player gd phisique

--- scripts/player.gd
+++ scripts/player.gd
@@ -14,30 +14,45 @@ var saved_direction: Vector3 = Vector3.ZERO
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity.y -= GRAVITY * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	var input_direction = get_input_direction()
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("left", "right", "forward", "backward")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	direction = direction.rotated(Vector3(0,1,0),Camera.rotation.y)
-	if direction:
-		if is_on_floor():
-			velocity.x = move_toward(velocity.x, direction.x * SPEED, ACCELERATION_MID_AIR)
-			velocity.z = move_toward(velocity.z, direction.z * SPEED, ACCELERATION_MID_AIR)
-		else:
-			velocity.x = move_toward(velocity.x, direction.x * SPEED, ACCELERATION_MID_AIR)
-			velocity.z = move_toward(velocity.z, direction.z * SPEED, ACCELERATION_MID_AIR)
+	if is_on_floor():
+
+		if Input.is_action_just_pressed("ui_accept"):
+			velocity.y = JUMP_VELOCITY
+			saved_direction = input_direction
+
+		elif input_direction == Vector3.ZERO:
+			velocity.x = move_toward(velocity.x, 0, HOP_FORCE * 0.3)
+			velocity.z = move_toward(velocity.z, 0, HOP_FORCE * 0.3)
+
+		elif not Input.is_action_pressed("ui_accept") and input_direction != Vector3.ZERO:
+			saved_direction = input_direction
+			var impulse = input_direction.normalized() * HOP_FORCE
+			velocity.x = impulse.x
+			velocity.z = impulse.z
+			velocity.y = HOP_HEIGHT
 	else:
-		if is_on_floor():
-			velocity.x -= velocity.x * DECELERATION_FLOOR * delta
-			velocity.z -= velocity.z * DECELERATION_FLOOR * delta
-		else:
-			velocity.x -= velocity.x * DECELERATION_MID_AIR * delta
-			velocity.z -= velocity.z * DECELERATION_MID_AIR * delta
+		# Contrôle en l'air
+		var blended = saved_direction.lerp(input_direction, AIR_CONTROL_BLEND).normalized()
+		velocity.x = move_toward(velocity.x, blended.x * HOP_FORCE, ACCELERATION_MID_AIR)
+		velocity.z = move_toward(velocity.z, blended.z * HOP_FORCE, ACCELERATION_MID_AIR)
 
 	move_and_slide()
+
+func get_input_direction() -> Vector3:
+	var input_vec = Input.get_vector("left", "right", "forward", "backward")
+	var dir = (transform.basis * Vector3(input_vec.x, 0, input_vec.y)).normalized()
+	return dir.rotated(Vector3.UP, Camera.rotation.y)
+	
+	
+	
+var is_pogo_on_floor := 0
+
+func _on_area_pogo_body_entered(_body: Node3D) -> void:
+	is_pogo_on_floor += 1
+
+func _on_area_pogo_body_exited(_body: Node3D) -> void:
+	is_pogo_on_floor -= 1
