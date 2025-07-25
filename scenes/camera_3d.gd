e3bc1fa0fd8f9fb15446074b59d737523be6523d
Gabanger <gabighost3491@gmail.com>
2025-07-25 10:20:41 +0200

    Raycast for Camera

--- scenes/camera_3d.gd
+++ scenes/camera_3d.gd
@@ -3,6 +3,10 @@ extends Camera3D
 const SENSITIVITY = 1.0/240.0
 const THIRD_PERSON_RANGE = 5
 const OFFSET_POS = Vector3(0,1.25,0)
+const DISTANCE_FROM_SURFACE = 0.05
+
+@onready var Raycast = $"../RayCast_Camera"
+@onready var Player = $".."
 
 var is_in_first_person = true
 
@@ -17,6 +21,10 @@ func _input(event: InputEvent) -> void:
 			is_in_first_person = not is_in_first_person
 			update_camera(event,false)
 
+func _process(_delta: float) -> void:
+	if not is_in_first_person:
+		update_camera(InputEventKey.new(), false)
+
 func update_camera(e : InputEvent, change_angle : bool):
 	if change_angle:
 		var MouseEvent = e.relative
@@ -24,9 +32,16 @@ func update_camera(e : InputEvent, change_angle : bool):
 		rotation.x = min(PI/2,rotation.x)
 		rotation.x = max(-PI/2,rotation.x)
 		rotation.y += MouseEvent.x * SENSITIVITY * -1
+	
 	if is_in_first_person:
 		position = OFFSET_POS
 	else:
 		var vec = global_basis * Vector3.FORWARD
 		vec *= -1 * THIRD_PERSON_RANGE
-		position = OFFSET_POS + vec
+		Raycast.target_position = vec
+		if Raycast.is_colliding():
+			var gap = Raycast.get_collision_normal()
+			gap *= DISTANCE_FROM_SURFACE
+			position = gap + Raycast.get_collision_point() - Player.position
+		else:
+			position = OFFSET_POS + vec
